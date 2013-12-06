b07b5a5bd0fe116fdd5dfc45a3f6bc0a399a7308
Roman Kolesnev <rvkolesnev@gmail.com>
2013-12-06 15:14:32 +0400

    Fix vitrual methos issue

--- matrix_object.hpp
+++ matrix_object.hpp
@@ -5,7 +5,7 @@ template<class number, int N, int M>
 class MatrixObject
 {
 public:
-	virtual const number get(const int i, const int j) const { return 0; }; // why i have linker error when make this method virual?
+	virtual const number get(const int i, const int j) const = 0; 
 };
 
 #endif
