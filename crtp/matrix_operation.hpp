c7aaf344bfb43b163e9f9190563a60b6dcb8ca52
Roman Kolesnev <rvkolesnev@gmail.com>
2013-12-25 21:06:45 +0400

    Remove unnecessary code

--- crtp/matrix_operation.hpp
+++ crtp/matrix_operation.hpp
@@ -13,13 +13,12 @@ struct SimpleMatrixOperation : public MatrixObject< SimpleMatrixOperation<A1, A2
 
 	const MatrixObject<A1, element>& arg1;
 	const MatrixObject<A2, element>& arg2;
-	const binary_f calc;
+	binary_f calc;
 
 	SimpleMatrixOperation(const MatrixObject<A1, element>& arg1, const MatrixObject<A2, element>& arg2) :
 		BaseType(arg1.getN(), arg1.getM()),
 		arg1(arg1),
-		arg2(arg2),
-		calc(binary_f())
+		arg2(arg2)
 	{
 		if ( (arg1.getN() != arg2.getN()) || (arg1.getM() != arg2.getM()))
 		{
